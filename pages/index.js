1c37173e0e6318d8d2b6a80a67ddf3339589cf53
dev <dev@MacBook-Pro-de-dev.local>
2018-03-26 16:06:27 +0200

    /about link client-side

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
+import Link from "next/link";
+
 const Index = () => (
   <div>
+    <Link href="/about">
+      <a>About Page</a>
+    </Link>
     <p>Hello Next.js</p>
-    <p>This is my first Next.js App!</p>
   </div>
 );
 
