877d35ef03e2d7d4eadde3291d4cdeabe7e30e62
dev <dev@MacBook-Pro-de-dev.local>
2018-03-27 13:07:38 +0200

    post.js props.url

--- pages/post.js
+++ pages/post.js
@@ -1,7 +1,7 @@
-import Layout from "../comps/MyLayout";
+import Layout from "../comps/MyLayout.js";
 
-export default props => (
-  <Layout>
+const Content = props => (
+  <div>
     {/* 
         - Every page will get a prop called “URL” which has some details related to the current URL.
         - In this case, we are using the “query” object, which has the query string params.
@@ -9,5 +9,21 @@ export default props => (
     */}
     <h1>{props.url.query.title}</h1>
     <p>This is the blog post content.</p>
+  </div>
+);
+
+export default props => (
+  <Layout>
+    <Content url={props.url} />
   </Layout>
 );
+
+
+{/* 
+    export default (props) => (
+        <Layout>
+            // To pass all props
+            <Content {...props}/>
+        </Layout>
+    ); 
+*/}
