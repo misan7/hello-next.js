61f0df82fae17304c913d2a05cf0a3e4dcf41541
dev <dev@MacBook-Pro-de-dev.local>
2018-03-27 12:35:29 +0200

    Dynamic pages

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,22 @@
 import Layout from "../comps/MyLayout.js";
+import Link from "next/link";
+
+const PostLink = props => (
+  <li>
+    <Link href={`/post?title=${props.title}`}>
+      <a>{props.title}</a>
+    </Link>
+  </li>
+);
 
 export default () => (
   <Layout>
-    <p>Hello Next.js</p>
+    <h1>My Blog</h1>
+    <ul>
+      <PostLink title="Hello Next.js" />
+      <PostLink title="Learn Next.js" />
+      <PostLink title="Deploy apps" />
+      <PostLink title="Dynamic Pages!" />
+    </ul>
   </Layout>
 );
