80b4bd96bf3bde06485d8473401740b56185c2a6
dev <dev@MacBook-Pro-de-dev.local>
2018-03-27 13:19:41 +0200

    Clean URLs with Route Masking using the 'as' prop.

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
 import Layout from "../comps/MyLayout.js";
 import Link from "next/link";
 
-const PostLink = (props) => (
+const PostLink = props => (
   <li>
-    <Link href={`/post?title=${props.title}`}>
+    <Link as={`/post/${props.id}`} href={`/post?title=${props.title}`}>
       <a>{props.title}</a>
     </Link>
   </li>
@@ -13,10 +13,10 @@ export default () => (
   <Layout>
     <h1>My Blog</h1>
     <ul>
-      <PostLink title="Hello Next.js" />
-      <PostLink title="Learn Next.js" />
-      <PostLink title="Deploy apps" />
-      <PostLink title="Dynamic Pages!" />
+      <PostLink id="hello-next.js" title="Hello Next.js" />
+      <PostLink id="Learn-next.js"title="Learn Next.js" />
+      <PostLink id="Deploy-apps"title="Deploy apps" />
+      <PostLink id="Dynamic-pages!"title="Dynamic Pages!" />
     </ul>
   </Layout>
 );
